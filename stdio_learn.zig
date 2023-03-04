bf6c775d6b6c6a20f19d11b03634aab24b007b4c
00JCIV00 <jake.crawfordiv@gmail.com>
2023-03-03 22:12:10 -0500

    Reformatted stdio_learn.zig
    - Formatted stdio_learn.zig with 'zig fmt'.

--- stdio_learn.zig
+++ stdio_learn.zig
@@ -9,22 +9,15 @@ pub fn main() !void {
     const open_msg: []const u8 =
         \\Let the mimicry begin!
         \\Exit with (c)lose, (e)xit, or (q)uit.
-		\\
+        \\
     ;
     try stdout.writeAll(open_msg);
     user_io: while (true) {
         const user_line: []const u8 = try stdin.readUntilDelimiterAlloc(std.heap.page_allocator, '\n', 8192);
         defer std.heap.page_allocator.free(user_line);
-		const exit_cmds = [_][]const u8 { 
-			"close", 
-			"exit", 
-			"quit", 
-			"c", 
-			"e", 
-			"q" 
-		};
-		for (exit_cmds) |cmd| if (equals(u8, cmd, user_line)) break :user_io;
-		try stdout.print("- Your Input: {s}\n", .{user_line});	
+        const exit_cmds = [_][]const u8{ "close", "exit", "quit", "c", "e", "q" };
+        for (exit_cmds) |cmd| if (equals(u8, cmd, user_line)) break :user_io;
+        try stdout.print("- Your Input: {s}\n", .{user_line});
     }
-	try stdout.writeAll("That's all for now!\n");
+    try stdout.writeAll("That's all for now!\n");
 }

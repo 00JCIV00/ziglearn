0c9f088b847e78504315ae392377fc37502a4c7f
00JCIV00 <68087632+00JCIV00@users.noreply.github.com>
2023-03-04 23:05:38 -0500

    Delete helloworld.zig

--- helloworld.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const print = std.debug.print;
-
-pub fn main() void {
-    const strDemo = .{ .h = "hello", .w = "World" };
-    var chgStr = strDemo.h.*;
-    chgStr[0] = 'H';
-    print("{s}, {s}!\n", .{ .h = chgStr, .w = strDemo.w });
-}

1b283194f620bb0165bbebe0ef4be8bc96799ee0
00JCIV00 <jake.crawfordiv@gmail.com>
2023-03-26 02:07:34 -0400

    Added recursive struct printing to types_learn.zig
    - Commented out UnionDemo Interface attempt.
    - Figured out how to distinguish structs from other types while writing out info.

--- solo_examples/types_learn.zig
+++ solo_examples/types_learn.zig
@@ -49,63 +49,123 @@ const PackedDemo2 = packed struct {
 	};
 };
 
-/// Union Demo. A Tagged (enum) Union can be used to replicate inheritance/interface-like concepts. 
-const UnionDemo = union(enum) {
-	pd1: PackedDemo,
-	pd2: PackedDemo2,
-	
-	// Use a configuration struct in place of default paramters.
-	const WriteInfoConfig = struct { 
-		prefix: []const u8 = "-",
-		depth: u8 = 0,
-		
-		fn getPrefix(self: *const WriteInfoConfig, alloc: *std.mem.Allocator) ![]const u8 {
-			return if (self.depth == 0) "" else newPrefix: {
-				var new_prefix: []u8 = "";
-				for (0..(self.depth)) |_| 
-					new_prefix = try std.fmt.allocPrint(alloc.*, "{s}{s}", .{new_prefix, self.prefix})
-				else break :newPrefix new_prefix;
-				
-			};
-		}
-	};
-	/// Pull Name, Type, and Size info from a Union Member (Packed Struct) and its Fields.
-	fn writeInfo(union_if: *UnionDemo, alloc: *std.mem.Allocator, writer: anytype, info_config: WriteInfoConfig) !void {
-		var info = info_config;
-		switch (union_if.*) {
-			inline else => |*self| {
-				// Use @TypeOf(obj) builtin to return the 'type' of any object.
-				const self_type = @TypeOf(self.*);
-				// Use @typeName(type), @sizeOf(type), or @bitSizeOf(type) for relevant type data.
-				try writer.print("{s} Struct = Type: {s}, Size: {d}B\n", .{try info.getPrefix(alloc), @typeName(self_type), @sizeOf(self_type)});
-				// Use std.meta.fields(type) to iterate over the fields of a struct.
-				//inline for(std.meta.fields(self_type)) |field|
-				//	try writer.print("- Field = Name: {s}, Type: {s}, Size: {d}b\n", .{field.name, @typeName(field.type), @bitSizeOf(field.type)});
-				
-				// ...OR...
+/// Union Demo. A Tagged (enum) Union can be used to replicate inheritance/interface-like concepts. However, this requires new objects to be created as a Union, not their raw type form, making it very limited (v11).
+//const UnionDemo = union(enum) {
+//	pd1: PackedDemo,
+//	pd1_in: PackedDemo.InnerPackedDemo,
+//	pd2: PackedDemo2,
+//	pd2_in: PackedDemo2.InnerPackedDemo,
+//	pd2_in_nest: PackedDemo2.InnerPackedDemo.InnerNestedStruct,
+//	
+//	/// Use a configuration struct in place of default paramters.
+//	const WriteInfoConfig = struct { 
+//		prefix: []const u8 = "-",
+//		depth: u8 = 0,
+//		
+//		fn getPrefix(self: *const WriteInfoConfig, alloc: *std.mem.Allocator) ![]const u8 {
+//			return if (self.depth == 0) "" else newPrefix: {
+//				var new_prefix: []u8 = "";
+//				for (0..(self.depth)) |_| 
+//					new_prefix = try std.fmt.allocPrint(alloc.*, "{s}{s}", .{new_prefix, self.prefix})
+//				else break :newPrefix new_prefix;
+//				
+//			};
+//		}
+//	};
+//	/// Pull Name, Type, and Size info from a Union Member (Packed Struct) and its Fields.
+//	fn writeInfo(union_if: *UnionDemo, alloc: *std.mem.Allocator, writer: anytype, info_config: WriteInfoConfig) !void {
+//		var info = info_config;
+//		switch (union_if.*) {
+//			inline else => |*self| {
+//				// Use @TypeOf(obj) builtin to return the 'type' of any object.
+//				const self_type = @TypeOf(self.*);
+//				// Use @typeName(type), @sizeOf(type), or @bitSizeOf(type) for relevant type data.
+//				try writer.print("{s} Struct = Type: {s}, Size: {d}B\n", .{try info.getPrefix(alloc), @typeName(self_type), @sizeOf(self_type)});
+//				// Use std.meta.fields(type) to iterate over the fields of a struct.
+//				//inline for(std.meta.fields(self_type)) |field|
+//				
+//				// ...OR...
+//
+//				// Use @typeInfo(type).Struct.fields to iterate over the fields of a struct.
+//				info.depth += 1;	
+//				inline for(@typeInfo(self_type).Struct.fields) |field| {
+//					inline for(@typeInfo(UnionDemo).Union.fields) |u_field| {
+//						if (u_field.type == field.type) {
+//							//const u_struct: u_field.type = @field(self.*, field.name);
+//							//u_struct.writeInfo(alloc, writer, info);
+//							try writeDemo(&field);
+//							break;
+//						}
+//					}
+//					else try writer.print("{s} Field = Name: {s}, Type: {s}, Size: {d}b, Value: {}\n", .{try info.getPrefix(alloc), field.name, @typeName(field.type), @bitSizeOf(field.type), @field(self.*, field.name)});
+//				}
+//			}
+//		}
+//	}
+//};
 
-				// Use @typeInfo(type).Struct.fields to iterate over the fields of a struct.
-				info.depth += 1;	
-				inline for(@typeInfo(self_type).Struct.fields) |field| {
-					try writer.print("{s} Field = Name: {s}, Type: {s}, Size: {d}b, Value: {}\n", .{try info.getPrefix(alloc), field.name, @typeName(field.type), @bitSizeOf(field.type), @field(self.*, field.name)});
-				}
-			}
-		}
+/// Use a configuration struct in place of default paramters.
+const WriteInfoConfig = struct { 
+	prefix: []const u8 = "-",
+	depth: u8 = 0,
+	
+	fn getPrefix(self: *const WriteInfoConfig, alloc: std.mem.Allocator) ![]const u8 {
+		return if (self.depth == 0) "" else newPrefix: {
+			var new_prefix: []u8 = "";
+			for (0..(self.depth)) |_| 
+				new_prefix = try std.fmt.allocPrint(alloc, "{s}{s}", .{new_prefix, self.prefix})
+			else break :newPrefix new_prefix;
+		};
 	}
 };
 
+/// Pull Name, Type, and Size info from a Struct and its Fields.
+fn writeInfo(ptr: anytype, alloc: std.mem.Allocator, writer: anytype, info_config: WriteInfoConfig) !void {
+	var info = info_config;
+	const T = std.meta.Child(@TypeOf(ptr));
+	const self = @ptrCast(*T, @constCast(ptr));
+	const fields = std.meta.fields(T);
+
+	try writer.print("{s} Struct = Type: {s}, Size: {d}B, # Fields: {d}\n", 
+		.{ 
+			try info.getPrefix(alloc), 
+			@typeName(T), 
+			@sizeOf(T),
+			fields.len,
+		}
+	);
+
+	info.depth += 1;
+	inline for (fields) |field| {
+		const field_self = @field(self.*, field.name);
+		if (@typeInfo(field.type) == .Struct) try writeInfo(&field_self, alloc, writer, info)
+		else try writer.print("{s} Field = Name: {s}, Type: {s}, Size: {d}b, Value: {any}\n",
+			.{ 
+				try info.getPrefix(alloc), 
+				field.name, 
+				@typeName(field.type), 
+				@bitSizeOf(field.type), 
+				field_self
+			}
+		);
+	}
+}
+
 pub fn main() !void {
+	try stdout.writeAll("Starting...\n\n");
 	var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     var allocator = arena.allocator();
 
-	var demo_ps = UnionDemo { .pd1 = .{} };
-	var demo_ps2 = UnionDemo { .pd2 = .{} };
+	var demo_ps: PackedDemo = .{};
+	var demo_ps2: PackedDemo2 = .{};
 	try stdout.writeAll("PackedDemo:\n");
-	try demo_ps.writeInfo(&allocator, &stdout, .{.depth = 1});
-	try stdout.writeAll("PackedDemo2:\n");
-	try demo_ps2.writeInfo(&allocator, &stdout, .{});
+	try writeInfo(&demo_ps, allocator, &stdout, .{ .depth = 0 });
+	try stdout.writeAll("\nPackedDemo2:\n");
+	try writeInfo(&demo_ps2, allocator, &stdout, .{});
 	
 	// Print the full contents of a struct. (Can this be formatted somehow?)
 	//try stdout.print("PackedDemo Struct:\n{}\n", .{demo_ps});
+
+	try stdout.writeAll("\nFinished.\n");
 }
